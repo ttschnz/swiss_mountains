b947266bb7f7065ced292606f0af6aea96d39dda
Tim T. <ttschnz@protonmail.com>
2025-08-28 17:24:30 +0200

    move dpi to config

--- config/constants.py
+++ config/constants.py
@@ -1,2 +1,3 @@
 FOCAL_LENGTH = 0.2 # default value would be 1. values smaller than 1 create a larger field of view
 ELEVATION_ANGLE = 30 # angle at which the animation turns. 0 is straight
+DPI = 300 # dots per inches for exports
