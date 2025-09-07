8ff50b418e07e20e25bfc75ce0b827c412930d30
Tim T. <ttschnz@protonmail.com>
2025-09-07 23:54:16 +0200

    use rayon for mutlithreading

--- rust/src/terrain_viz/create_mesh.rs
+++ rust/src/terrain_viz/create_mesh.rs
@@ -2,34 +2,40 @@ use super::{AltitudeData, ImageData};
 
 use anyhow::Result;
 use delaunator::{triangulate, Point};
-use kiddo::float::kdtree::KdTree;
+use kiddo::immutable::float::kdtree::ImmutableKdTree;
 use kiddo::SquaredEuclidean;
 use log::debug;
+use rayon::prelude::*;
 use three_d::*;
 
 pub fn create_mesh(altitude_data: AltitudeData, image_data: ImageData) -> Result<CpuMesh> {
     // build kd-tree for color lookup
-    // create color map for lookup
-    let mut tree: KdTree<f32, u64, 2, 1024, u32> = KdTree::with_capacity(image_data.len());
-
     debug!("filling kdtree");
-    let mut colors: Vec<Srgba> = Vec::new();
-    for (i, (x, z, r, g, b)) in image_data.iter().enumerate() {
-        tree.add(&[*x as f32, *z as f32], i as u64);
-        colors.push(Srgba::new_opaque(*r, *g, *b));
-    }
+    let tree: ImmutableKdTree<f32, u64, 2, 4096> = ImmutableKdTree::new_from_slice(
+        &image_data
+            .par_iter()
+            .map(|(x, z, _r, _g, _b)| [*x as f32, *z as f32])
+            .collect::<Vec<_>>(),
+    );
+
+    // create color map for lookup
+    debug!("filling color map");
+    let colors = image_data
+        .par_iter()
+        .map(|(_x, _z, r, g, b)| Srgba::new_opaque(*r, *g, *b))
+        .collect::<Vec<_>>();
 
     // convert altitude data to vertex positions
     debug!("converting altitude data to vertices");
     let vertices: Vec<Vec3> = altitude_data
-        .iter()
+        .par_iter()
         .map(|(x, z, y)| vec3(*x as f32, *y, *z as f32))
         .collect();
 
     // Perform Delaunay triangulation in XZ plane
     debug!("creating triangles");
     let points: Vec<Point> = altitude_data
-        .iter()
+        .par_iter()
         .map(|(x, z, _)| Point {
             x: *x as f64,
             y: *z as f64,
@@ -47,12 +53,13 @@ pub fn create_mesh(altitude_data: AltitudeData, image_data: ImageData) -> Result
     // find closest points to each corner (vertex)
     // as the grids don't necessarily align, we find the closest image point in the xz-plane
     debug!("coloring mesh");
-    let mut vertex_colors = Vec::new();
-    for (x, z, _y) in altitude_data.iter() {
-        let nearest_color_index = tree.nearest_one::<SquaredEuclidean>(&[*x as f32, *z as f32]);
-        let color: Srgba = colors[nearest_color_index.item as usize];
-        vertex_colors.push(color);
-    }
+    let vertex_colors = altitude_data
+        .par_iter()
+        .map(|(x, z, _y)| {
+            let nearest_color_index = tree.nearest_one::<SquaredEuclidean>(&[*x as f32, *z as f32]);
+            colors[nearest_color_index.item as usize]
+        })
+        .collect::<Vec<_>>();
 
     // build CPU mesh
     debug!("building mesh");
