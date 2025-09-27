8b4ff50c70ed34c011c4eabc3e1c1af1ad8aa11c
Tim T. <ttschnz@protonmail.com>
2025-09-27 19:44:54 +0200

    speed up gif with multithread

--- rust/src/terrain_viz/compose_gif.rs
+++ rust/src/terrain_viz/compose_gif.rs
@@ -1,17 +1,22 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use gif::{Encoder, Frame, Repeat};
+use rayon::prelude::*;
 use std::fs::File;
 
 pub fn compose_gif(paths: &[String], output_path: &str, size: (u16, u16)) -> Result<()> {
+
+    let frames = paths.par_iter().map(|path| {
+        let img = image::open(&path)?.to_rgba8();
+        let mut raw_pixels = img.into_raw();
+        Ok::<Frame<'_>, Error>(Frame::from_rgba_speed(size.0, size.1, &mut raw_pixels, 10))
+    }).collect::<Vec<_>>();
+
     let mut image_file = File::create(output_path)?;
     let mut encoder = Encoder::new(&mut image_file, size.0, size.1, &[])?;
     encoder.set_repeat(Repeat::Infinite)?;
 
-    for path in paths {
-        let img = image::open(&path)?.to_rgba8();
-        let mut raw_pixels = img.into_raw();
-        let frame = Frame::from_rgba_speed(size.0, size.1, &mut raw_pixels, 10);
-        encoder.write_frame(&frame)?;
+    for frame in frames{
+        encoder.write_frame(&frame?)?;
     }
 
     Ok(())
